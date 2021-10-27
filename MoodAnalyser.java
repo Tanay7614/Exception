fc1facca259e0773da0c9df4c079c7adfcfda557
Tanay7614 <86945887+Tanay7614@users.noreply.github.com>
2021-10-27 14:51:50 +0530

    Return sad msg

---
+++ MoodAnalyser.java
@@ -0,0 +1,15 @@
+package sample;
+
+public class MoodAnalyser {
+
+	public String analyseMood(String msg) {
+		if(msg.contains("sad")) {
+			return "sad";
+		}
+		else {
+		return "Happy";
+		}
+		
+	}
+
+}
