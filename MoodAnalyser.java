a15fc00388ada9daddaf8d22e5663756bf136982
Tanay7614 <86945887+Tanay7614@users.noreply.github.com>
2021-10-27 15:50:32 +0530

    Null_pointer_exception

---
+++ MoodAnalyser.java
@@ -0,0 +1,28 @@
+package sample;
+
+public class MoodAnalyser {
+    String msg;
+	
+    public MoodAnalyser(String msg)
+    {
+		this.msg=msg;
+	}
+	
+	public String analyseMood() {
+		try {
+		if(msg.contains("sad")) 
+		{
+			return "sad";
+		}
+		else
+		{
+		return "Happy";
+		}
+		}catch(NullPointerException e) {
+			
+		}
+		return "Happy";
+		
+	}
+
+}
