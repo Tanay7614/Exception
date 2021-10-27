76e6685934fb5595b34c037b49a4f82b2c97871b
Tanay7614 <86945887+Tanay7614@users.noreply.github.com>
2021-10-27 14:56:34 +0530

    Return Happy msg

--- Mood.java
+++ Mood.java
@@ -3,7 +3,7 @@ package sample;
 public class Mood {
 	public static void main(String[] args) {
 		MoodAnalyser moodAnalyser =new MoodAnalyser ();
-		String mood= moodAnalyser.analyseMood("I am in sad Mood ");
+		String mood= moodAnalyser.analyseMood("I am in any Mood ");
 		String expectedValue="sad";
 		System.out.println(mood);
 		
